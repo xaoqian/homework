ca280daf00ca066b557908b3367679bc9ee6023d
haoqian <33185351+xaoqian@users.noreply.github.com>
2018-01-08 19:55:15 +0300

    Add files via upload

---
+++ pack_другие интересные задачи/сколько сейчас времени.py
@@ -0,0 +1,8 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+KeyboardInterrupt
+>>> import time
+print("Start : ",time.ctime())
+time.sleep(5)
+print("End : ",time.ctime())
