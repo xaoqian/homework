df5ac7df5939ac75b458c65c16dda54795e23371
haoqian <33185351+xaoqian@users.noreply.github.com>
2018-01-08 19:55:50 +0300

    Update построить основный график.py

--- pack_другие интересные задачи/построить основный график.py
+++ pack_другие интересные задачи/построить основный график.py
@@ -1,6 +1,4 @@
-Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> import turtle
+import turtle
 turtle.forward(100)
 turtle.right(60)
 turtle.forward(100)
